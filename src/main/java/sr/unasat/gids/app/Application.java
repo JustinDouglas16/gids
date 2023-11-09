32abab02f27982c13e6efc3d37105217809f4fb8
JustinDouglas16 <justinrafael.douglas@gmail.com>
2023-11-09 20:00:18 -0300

    Hibernate Implementations

--- src/main/java/sr/unasat/gids/app/Application.java
+++ src/main/java/sr/unasat/gids/app/Application.java
@@ -5,9 +5,7 @@ import sr.unasat.gids.repositories.AdresRepository;
 
 public class Application {
     public static void main(String[] args) {
-//        System.out.println("hello");
-
-        Adres adres = new Adres(1, "Hindilaan");
+        Adres adres = new Adres(2, "Hindilaan");
         AdresRepository adresRepository = new AdresRepository();
         adresRepository.insertOneRecord(adres);
     }
